e3415e49194ff7c06782d904328ac9021ef9db16
felipetempus <felipemouradev@gmail.com>
2020-08-07 16:58:49 -0400

    chap 8

--- libraryapp/views/books/form.py
+++ libraryapp/views/books/form.py
@@ -21,6 +21,28 @@ def get_libraries():
 
         return db_cursor.fetchall()
 
+
+def get_book(book_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Book)
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            b.id,
+            b.title,
+            b.isbn,
+            b.author,
+            b.year_published,
+            b.librarian_id,
+            b.location_id
+        FROM libraryapp_book b
+        WHERE b.id = ?
+        """, (book_id,))
+
+        return db_cursor.fetchone()
+
+
 @login_required
 def book_form(request):
     if request.method == 'GET':
@@ -32,22 +54,18 @@ def book_form(request):
 
         return render(request, template, context)
     
-    # elif request.method == 'POST':
-    #     form_data = request.POST
 
-    #     with sqlite3.connect(Connection.db_path) as conn:
-    #         db_cursor = conn.cursor()
+@login_required
+def book_edit_form(request, book_id):
 
-    #         db_cursor.execute("""
-    #         INSERT INTO libraryapp_book
-    #         (
-    #             title, author, isbn,
-    #             year_published, location_id, librarian_id
-    #         )
-    #         VALUES (?, ?, ?, ?, ?, ?)
-    #         """,
-    #         (form_data['title'], form_data['author'],
-    #             form_data['isbn'], form_data['year_published'],
-    #             request.user.librarian.id, form_data["location"]))
+    if request.method == 'GET':
+        book = get_book(book_id)
+        libraries = get_libraries()
 
-    # return redirect(reverse('libraryapp:books'))
+        template = 'books/form.html'
+        context = {
+            'book': book,
+            'all_libraries': libraries
+        }
+
+        return render(request, template, context)
