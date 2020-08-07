9769b4627a99567d6dda0d6463051dd359081891
felipetempus <felipemouradev@gmail.com>
2020-08-06 20:40:11 -0400

    just saying

--- libraryapp/views/libraries/form.py
+++ libraryapp/views/libraries/form.py
@@ -9,20 +9,3 @@ from ..connection import Connection
 def library_form(request):
     if request.method == 'GET':
         return render(request, 'libraries/form.html', {})
-    
-    elif request.method == 'POST':
-        form_data = request.POST
-
-        with sqlite3.connect(Connection.db_path) as conn:
-            db_cursor = conn.cursor()
-
-            db_cursor.execute("""
-            INSERT INTO libraryapp_library
-            (
-                title, address
-            )
-            VALUES (?, ?)
-            """,
-            (form_data['title'], form_data['address']))
-
-        return redirect(reverse('libraryapp:libraries'))
